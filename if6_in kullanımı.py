91f0aab1062ea899a161cc2495fe850dbefb24cf
Oguzsfrn <oguzsfrn910@gmail.com>
2025-02-12 20:28:05 +0300

    yenileme

---
+++ if6_in kullanımı.py
@@ -0,0 +1,7 @@
+corbalar = ["Tarhana","Mercimek","Şehriye"]
+
+print("Paça" in corbalar)
+print("Tarhana" in corbalar)
+
+if "Paça" in corbalar : print("Paça menümüzde var")
+else : print
---
+++ if8_ternary kullanımı.py
@@ -0,0 +1,5 @@
+print("xx") if 10 < 20 else print("yy")
+
+ort = 60 
+print("geçti" if ort>49 else "kaldı")
+
