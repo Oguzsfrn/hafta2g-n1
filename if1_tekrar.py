4bc7c344c66638fbf69548908b2618475b7596a9
Oguzsfrn <oguzsfrn910@gmail.com>
2025-02-12 19:50:10 +0300

    tekrar

--- if1_tekrar.py
+++ if1_tekrar.py
@@ -1,7 +1,7 @@
 a = 7
 
-# if True : print("çalıştı1")
-# if True : print("çalıştı2")
+# if True : print("çalıştı1") # if ten sonraki ifade true ise çaısır
+# if False : print("çalıştı2") # if ten sonraki ifade folse ise çalişmaz
 
 print(a>5)
 print(a>3)
