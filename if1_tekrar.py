b1c86c54b786d19afabbe06400d363443af8be40
Oguzsfrn <oguzsfrn910@gmail.com>
2025-02-12 19:43:27 +0300

    ifkonusu

---
+++ if1_tekrar.py
@@ -0,0 +1,13 @@
+a = 7
+
+# if True : print("çalıştı1")
+# if True : print("çalıştı2")
+
+print(a>5)
+print(a>3)
+
+# if a >5 : print("a 5ten büyük")
+# if a >3 : print("a 3ten büyük")
+
+if a>3 : print("a 3ten büyük")
+elif a>5 : print("a 5ten büyük")
